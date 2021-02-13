d43509eb6fd5f355ed9460cef9ecadc7421e8a92
Alejandro-Duenas-Garcia <alejandro.duenas@davivienda.com>
2021-02-13 14:54:05 -0500

    process_data file completed

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,53 @@
-import sys
+#----------------------------- Process Data File--------------------------------
 
+# 1. Import libraries:
+
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+
+# 2. Define basic functions:
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''This function loads the datasets with the messages and categories
+    Inputs:
+        messages_filepath: str with the file path of the CSV file containing the 
+        messages data
+        categories_filepath: str with the file ppath of the CSV file containing
+        the categories data 
+    Outputs:
+        pandas Dataframe       
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages,categories
 
 
 def clean_data(df):
-    pass
+    '''Cleans data of the input dataframe, eliminating duplicate values
+    Input:
+        df: Pandas DataFrame instance
+    Output:
+        df: Pandas DataFrame instance (withoug duplicates)
+    '''
+    # Remove duplicates
+    df.drop_duplicates(subset='message',keep='first',inplace=True)
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename='message_data'):
+    '''This function saves the df dataframe into a SQL file
+    Inputs:
+        df: Pandas DataFrame instance
+        database_filename: string containing the name of the SQL table saved
+        with a default value "message_data"
+    Outputs:
+        None
+    '''
+    engine = create_engine('sqlite:///{}.db'/forma(database_filename)) 
+    df.to_sql(database_filename,engine,index=False)
 
 
 def main():
